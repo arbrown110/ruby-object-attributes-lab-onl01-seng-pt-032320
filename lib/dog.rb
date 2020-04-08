cdb041519e7fcfadc8961801bf65b746c090dbc6
chic-caller-7520 <arbrown110@gmail.com>
2020-04-08 00:47:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,20 +3,11 @@ class Dog
   def intialize(name)
    @name = name
   end
+   def name
+    @name
+  end  
   def name=(new_dog)
     @name = new_dog
   end 
-  def name
-    @name
-  end  
-  
-
-
-
-
-
-
-
-
 end  
 
