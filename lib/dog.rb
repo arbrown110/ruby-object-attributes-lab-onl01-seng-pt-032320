626ff765908d6e90a02a0c2ea8810c5832b6e903
chic-caller-7520 <arbrown110@gmail.com>
2020-04-08 01:02:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,16 @@
 class Dog 
 
-
    def name
     @name
   end  
   def name=(new_dog)
     @name = new_dog
   end 
-  def 
+  def breed 
+    @breed
+  end 
+  def breed=(new_breed)
+    @breed = new_breed
+  end  
 end  
 
